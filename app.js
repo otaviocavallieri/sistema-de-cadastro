ff275513f5c657f20de922db2371bcaf124c440e
Otávio Cavallieri <otaviocavallie@gmail.com>
2023-07-19 16:34:36 -0300

    exercício da aula de commit

--- app.js
+++ app.js
@@ -1,2 +1,4 @@
 console.log("Rodando o sistema de gerenciamento de jogos");
 console.log("A aplicação está rodando no banco de dados");
+// mais um console de log de teste:
+console.log("O Otávio fez esse commit como exercício da aula");
